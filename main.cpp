5d81de27351df530b9a958682eae333dcddb9611
ArthurGoodman <arthur.goodman3@gmail.com>
2016-09-24 18:46:18 +0300

    tree pairing

--- main.cpp
+++ main.cpp
@@ -15,12 +15,12 @@ std::pair<BigInt, BigInt> unpair(const BigInt &z) {
     return std::make_pair(x, y);
 }
 
-BigInt modPair(BigInt x, BigInt y, BigInt mod) {
+BigInt modPair(const BigInt &x, const BigInt &y, const BigInt &mod) {
     return mod * y + x;
 }
 
-std::pair<BigInt, BigInt> modUnpair(BigInt z, BigInt mod) {
-    std::pair<BigInt, BigInt> result = z.divide(mod);
+std::pair<BigInt, BigInt> modUnpair(const BigInt &z, const BigInt &mod) {
+    const std::pair<BigInt, BigInt> &result = z.divide(mod);
     return std::make_pair(result.second, result.first);
 }
 
@@ -33,21 +33,21 @@ BigInt pairVector(const std::vector<BigInt> &vector) {
     return res;
 }
 
-std::vector<BigInt> unpairVector(BigInt c, int size) {
+std::vector<BigInt> unpairVector(BigInt z, int size) {
     std::vector<BigInt> vector;
 
     for (int i = 0; i < size - 1; i++) {
-        std::pair<BigInt, BigInt> pair = unpair(c);
+        std::pair<BigInt, BigInt> pair = unpair(z);
         vector.push_back(pair.first);
-        c = pair.second;
+        z = pair.second;
     }
 
-    vector.push_back(c);
+    vector.push_back(z);
 
     return vector;
 }
 
-BigInt modPairVector(const std::vector<BigInt> &vector, BigInt mod) {
+BigInt modPairVector(const std::vector<BigInt> &vector, const BigInt &mod) {
     BigInt res = vector.back();
 
     for (int i = vector.size() - 2; i >= 0; i--)
@@ -56,18 +56,125 @@ BigInt modPairVector(const std::vector<BigInt> &vector, BigInt mod) {
     return res;
 }
 
-std::vector<BigInt> modUnpairVector(BigInt c, int size, BigInt mod) {
+std::vector<BigInt> modUnpairVector(BigInt z, int size, const BigInt &mod) {
     std::vector<BigInt> vector;
 
     for (int i = 0; i < size - 1; i++) {
-        std::pair<BigInt, BigInt> pair = modUnpair(c, mod);
+        std::pair<BigInt, BigInt> pair = modUnpair(z, mod);
         vector.push_back(pair.first);
-        c = pair.second;
+        z = pair.second;
     }
 
-    vector.push_back(c);
+    vector.push_back(z);
 
     return vector;
+}
+
+BigInt pairTree(const std::vector<BigInt> &vector) {
+    if (vector.empty())
+        return (ulong)0;
+
+    if (vector.size() == 1)
+        return vector.back();
+
+    if (vector.size() == 2)
+        return pair(vector[0], vector[1]);
+
+    if (vector.size() == 3)
+        return pair(vector[0], pair(vector[1], vector[2]));
+
+    std::vector<BigInt> lo, hi;
+    int i = 0;
+
+    for (; i < (int)vector.size() / 2; i++)
+        lo.push_back(vector[i]);
+
+    for (; i < (int)vector.size(); i++)
+        hi.push_back(vector[i]);
+
+    return pair(pairTree(lo), pairTree(hi));
+}
+
+std::vector<BigInt> unpairTree(const BigInt &z, int size) {
+    if (size == 0)
+        return {};
+
+    if (size == 1)
+        return {z};
+
+    const std::pair<BigInt, BigInt> &pair = unpair(z);
+
+    if (size == 2)
+        return {pair.first, pair.second};
+
+    if (size == 3) {
+        const std::pair<BigInt, BigInt> &temp = unpair(pair.second);
+        return {pair.first, temp.first, temp.second};
+    }
+
+    std::vector<BigInt> lo = unpairTree(pair.first, size / 2), hi = unpairTree(pair.second, size - size / 2);
+
+    std::vector<BigInt> result;
+
+    result.insert(result.end(), lo.begin(), lo.end());
+    result.insert(result.end(), hi.begin(), hi.end());
+
+    return result;
+}
+
+BigInt modPairTree(const std::vector<BigInt> &vector, const BigInt &mod) {
+    if (vector.empty())
+        return (ulong)0;
+
+    if (vector.size() == 1)
+        return vector.back();
+
+    if (vector.size() == 2)
+        return modPair(vector[0], vector[1], mod);
+
+    if (vector.size() == 3)
+        return modPair(vector[0], modPair(vector[1], vector[2], mod), mod);
+
+    std::vector<BigInt> lo, hi;
+    int i = 0;
+
+    for (; i < (int)vector.size() / 2; i++)
+        lo.push_back(vector[i]);
+
+    for (; i < (int)vector.size(); i++)
+        hi.push_back(vector[i]);
+
+    return pair(modPairTree(lo, mod), modPairTree(hi, mod));
+}
+
+std::vector<BigInt> modUnpairTree(const BigInt &z, int size, const BigInt &mod) {
+    if (size == 0)
+        return {};
+
+    if (size == 1)
+        return {z};
+
+    if (size == 2) {
+        const std::pair<BigInt, BigInt> &xy = modUnpair(z, mod);
+        return {xy.first, xy.second};
+    }
+
+    if (size == 3) {
+        const std::pair<BigInt, BigInt> &xw = modUnpair(z, mod);
+        const std::pair<BigInt, BigInt> &yz = modUnpair(xw.second, mod);
+        return {xw.first, yz.first, yz.second};
+    }
+
+    const std::pair<BigInt, BigInt> &xy = unpair(z);
+
+    std::vector<BigInt> lo = modUnpairTree(xy.first, size / 2, mod), hi = modUnpairTree(xy.second, size - size / 2, mod);
+
+    std::vector<BigInt> result;
+
+    result.insert(result.end(), lo.begin(), lo.end());
+    result.insert(result.end(), hi.begin(), hi.end());
+
+    return result;
 }
 
 template <class T>
@@ -77,11 +184,26 @@ void printVector(const std::vector<T> &vector) {
 }
 
 int main() {
-    std::vector<BigInt> vector = {(ulong)0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::vector<BigInt> vector = {3, 157, 84, 214, 128, 197, 38, 140, 91, 44};
 
     BigInt x = pairVector(vector);
     std::cout << x << "\n";
     printVector(unpairVector(x, vector.size()));
+    std::cout << "\n";
+
+    x = pair(modPairVector(vector, 256), 256);
+    std::cout << x << "\n";
+    printVector(modUnpairVector(unpair(x).first, vector.size(), 256));
+    std::cout << "\n";
+
+    x = pairTree(vector);
+    std::cout << x << "\n";
+    printVector(unpairTree(x, vector.size()));
+    std::cout << "\n";
+
+    x = pair(modPairTree(vector, 256), 256);
+    std::cout << x << "\n";
+    printVector(modUnpairTree(unpair(x).first, vector.size(), 256));
     std::cout << "\n";
 
     return 0;
